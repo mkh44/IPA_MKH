cf50fe8a42664cf02e4901887617330951d0a39b
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-22 14:20:51 +0100

    Greyscaling and showing image

--- part_1_filters.py
+++ part_1_filters.py
@@ -3,8 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import skimage as sk
 
+# loading image
 img = sk.io.imread('my_face.jpg')
 
+#resizing image
 img_resized = sk.transform.resize(img, (512, 512), anti_aliasing=True)
 
+#converting to greyscale
+gray_img = sk.color.rgb2gray(img_resized)
 
+#desplaying images
+plt.plot(img)
+plt.plot(img_resized)
+plt.plot(gray_img)
+plt.show()
