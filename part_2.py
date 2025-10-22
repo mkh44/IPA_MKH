be0aeab7ac98da362a2073cef49ac53461fa2a7c
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-22 16:48:15 +0100

    template for part 2

---
+++ part_2.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage as sk
+
+#load images
+
+#convert to greyscale
+
+#get rid of noise with gaussian filter
+
+#normalise images
+
+#automated template matching with rotation (to do)
+
+#find match location
+
+#find threshold
+
+#plotting
+
