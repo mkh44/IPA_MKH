15f2f325653acf0870d6874f54f650f270900459
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-22 17:49:15 +0100

    code for matching

--- part_2.py
+++ part_2.py
@@ -18,20 +18,33 @@ source_smooth = sk.filters.gaussian(source_grey, sigma=sigma_source)
 template_smooth = sk.filters.gaussian(template_grey, sigma=sigma_template)
 
 #normalise images
+source_smooth = sk.exposure.rescale_intensity(source_smooth, out_range=(0, 1))
+template_smooth = sk.exposure.rescale_intensity(template_smooth, out_range=(0, 1))
+
 
 #automated template matching with rotation (to do)
-#get angles
-#find best score
-#find best match
-#find best angle
 
-#for angles match with template
+angles = np.arange(0, 360, 10)
+best_score = -np.inf
+best_match = None
+best_angle = None
 
-#update scores
+for angle in angles:
+    rotated = sk.transform.rotate(source_smooth, angle, resize=False)
+    result = sk.feature.match_template(rotated, template_smooth, pad_input=True)
+    score = np.max(result)
+
+    if score > best_score:
+        best_score = score
+        best_angle = angle
+        best_match = result
 
 #find match location
+ij = np.unravel_index(np.argmax(best_match), best_match.shape)
+x, y, = ij[::-1]
 
 #find threshold
+
 
 #plotting
 
