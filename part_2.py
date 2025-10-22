202ce9275e652f10142fb17a2956013f386d86b1
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-22 16:53:56 +0100

    loaded images into code

--- part_2.py
+++ part_2.py
@@ -5,8 +5,11 @@ import skimage as sk
 
 #load images
 source = sk.io.imread('avian_blood.jpg')
+template = sk.io.imread('avian_blood_template.jpg')
 
 #convert to greyscale
+source_grey = sk.color.rgb2gray(source)
+template_grey = sk.color.rgb2gray(template)
 
 #get rid of noise with gaussian filter
 
