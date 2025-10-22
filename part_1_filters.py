4dbc5a5639476023d05bdbcead3cc2c0481e4bd9
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-22 14:17:17 +0100

    resizing image

--- part_1_filters.py
+++ part_1_filters.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import skimage as sk
 
-img = io.imread('my_face.jpg')
+img = sk.io.imread('my_face.jpg')
 
-img_resized = transform.resize(img, (512, 512), anti_aliasing=True)
+img_resized = sk.transform.resize(img, (512, 512), anti_aliasing=True)
+
+
