2648ed8fc06079bba88ac5406129df2951af4b4f
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-23 15:30:20 +0100

    added addaptive threshold selecting

--- part_2_b.py
+++ part_2_b.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, color, feature, transform, exposure
-from skimage.feature import match_template
+from skimage.feature import match_template, peak_local_max
 from skimage.draw import rectangle_perimeter
 
 #load images
@@ -25,3 +25,15 @@ template_edges = feature.canny(template_grey, sigma=2)
 scale_factor = 0.3
 source_edges_small = transform.rescale(source_edges, scale_factor, anti_aliasing=False)
 template_edges_small = transform.rescale(template_edges, scale_factor, anti_aliasing=False)
+
+# template matching (no rotation)
+result = match_template(source_edges_small, template_edges_small)
+
+# adaptive threshold selection
+all_scores = result.ravel()
+adaptive_threshold = np.mean(all_scores) + 2 * np.std(all_scores)
+
+
+
+
+
