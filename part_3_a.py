f683969bb43b9223a48603e26f4ffac12611f3e3
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-23 17:35:50 +0100

    loaded image of cells and resized and greyscaled

--- part_3_a.py
+++ part_3_a.py
@@ -1,10 +1,22 @@
 #PART 3A
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import io, color, filters, morphology, exposure, transform, measure, segmentation
+
+from part_1_filters import img_resized
 
 # load and resize
+img = io.imread('HeLa_cells.jpg')
+img_resized = transform.resize(img, (512, 512), anti_aliasing=True)
 
 # convert to greyscale
+img_grey = color.rgb2gray(img_resized)
 
-#denoise
+#DENOISE
+#enhance local contrast
+image_eq = exposure.equalize_adapthist(img_resized, clip_limit=0.03)
+
+#gaussian
 
 # enhance contrast#
 
