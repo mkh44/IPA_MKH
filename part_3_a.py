6f49fd2ca8483dd8d94a1a14eeb4a2ef5b45ecaf
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-23 17:30:27 +0100

    scaffolding

--- part_3_a.py
+++ part_3_a.py
@@ -1,1 +1,22 @@
 #PART 3A
+
+# load and resize
+
+# convert to greyscale
+
+#denoise
+
+# enhance contrast#
+
+#thresholding
+
+#remove small noise and fill small holes
+
+#separate touching cells
+
+#remove boundary touching objects
+
+#count labelled regions
+
+
+
