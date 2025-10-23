45bf24365de2cdc3d65cac7e9345318ab9e9c261
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-23 17:41:27 +0100

    cleanup

--- part_3_a.py
+++ part_3_a.py
@@ -2,8 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import io, color, filters, morphology, exposure, transform, measure, segmentation
-
-from part_1_filters import img_resized
+from scipy import ndimage as ndi
 
 # load and resize
 img = io.imread('HeLa_cells.jpg')
@@ -25,8 +24,18 @@ threshold_value = filters.threshold_otsu(img_smooth)
 binary = img_smooth > threshold_value
 
 #remove small noise and fill small holes
+binary_cleaned = morphology.remove_small_objects(binary, min_size=50)
+binary_cleaned = morphology.remove_small_holes(binary_cleaned, area_threshold=100)
 
 #separate touching cells
+distance = ndi.distance_transform_edt(binary_cleaned)
+
+#local max and min
+footprint_size = int(np.mean(img_eq.shape) / 50)
+local_max = morphology.local_maxima(distance, footprint=morphology.disk(footprint_size))
+
+markers, _ = ndi.label(local_max)
+labels = morphology.watershed(-distance, markers, mask=binary_cleaned)
 
 #remove boundary touching objects
 
