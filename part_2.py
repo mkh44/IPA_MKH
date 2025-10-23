eae514f941c1458de4e3d160cfa95cb8b4ab84c0
mkh44 <molly.hurley24@mail.dcu.ie>
2025-10-23 14:48:28 +0100

    used histogram equalising instead for normalisation and changed import module callings

--- part_2.py
+++ part_2.py
@@ -1,31 +1,27 @@
-
+#PART 2A
 import numpy as np
 import matplotlib.pyplot as plt
-import skimage as sk
+from skimage import io, color, feature, transform, exposure
+from skimage.feature import match_template
+from skimage.draw import rectangle_perimeter
 
 #load images
-source = sk.io.imread('avian_blood.jpg')
-template = sk.io.imread('avian_blood_template.jpg')
+source = io.imread('avian_blood.jpg')
+template = io.imread('avian_blood_template.jpg')
 
 #convert to greyscale
-source_grey = sk.color.rgb2gray(source)
-template_grey = sk.color.rgb2gray(template)
+source_grey = color.rgb2gray(source)
+template_grey = color.rgb2gray(template)
 
-#get rid of noise with gaussian filter
-sigma_source = np.std(source_grey) * 0.5
-sigma_template = np.std(template_grey) * 0.5
-source_smooth = sk.filters.gaussian(source_grey, sigma=sigma_source)
-template_smooth = sk.filters.gaussian(template_grey, sigma=sigma_template)
-
-#normalise images
-source_smooth = sk.exposure.rescale_intensity(source_smooth, out_range=(0, 1))
-template_smooth = sk.exposure.rescale_intensity(template_smooth, out_range=(0, 1))
+#normalise images to reduce noise
+source_norm = exposure.equalize_hist(source_grey)
+template_norm = exposure.equalize_hist(template_grey)
 
 #edge detection
-source_edges = sk.feature.canny(source)
+source_edges = feature.canny(source_norm, sigma=2)
+template_edges = feature.canny(template_norm, sigma=2)
 
-#automated template matching with rotation (to do)
-
+#rotation invariance
 angles = np.arange(0, 360, 15) #checking every 15 degrees
 best_score = -np.inf
 best_match = None
